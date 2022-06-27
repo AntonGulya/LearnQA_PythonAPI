cc813892cf82f7dfa37dd61928b3400b5e012dce
AntonGulya <98944597+AntonGulya@users.noreply.github.com>
2022-06-27 19:55:56 +0300

    EX5_JSON_Parsing

---
+++ 2. EX5_Request_JSON.py
@@ -0,0 +1,4 @@
+import json
+qtqq = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+obj = json.loads(qtqq)
+print(obj['messages'][1]['message'])
